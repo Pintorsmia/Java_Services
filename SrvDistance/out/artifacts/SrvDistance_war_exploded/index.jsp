8450260b36cfeabc1e1b15eac92a5774625a250f
Pintorsmia <janindamien@yahoo.fr>
2020-02-18 10:57:02 +0100

    Fonction distance OK

--- SrvDistance/out/artifacts/SrvDistance_war_exploded/index.jsp
+++ SrvDistance/out/artifacts/SrvDistance_war_exploded/index.jsp
@@ -11,6 +11,21 @@
     <title>$Title$</title>
   </head>
   <body>
-  $END$
+    <form action="" method="get" class="form-example">
+      <div class="form">
+        <label>Lat1 : </label>
+        <input type="text" name="lat1" id="lat1" required>
+        <label>Lon1 : </label>
+        <input type="text" name="lon1" id="lon1" required>
+        <label>Lat2 : </label>
+        <input type="text" name="lat2" id="lat2" required>
+        <label>Lon2 : </label>
+        <input type="text" name="lon2" id="lon2" required>
+      </div>
+      <div class="form">
+        <input type="submit" value="Recherche">
+      </div>
+    </form>
+
   </body>
 </html>
--- SrvDistance/src/calcul/Distance.java
+++ SrvDistance/src/calcul/Distance.java
@@ -6,11 +6,21 @@ import javax.xml.ws.Endpoint;
 @WebService()
 public class Distance {
     @WebMethod
-    public Double Distance(Float latA, Float lonA, Float latB, Float lonB) {
+    public Double Distance(Float lat1, Float lon1, Float lat2, Float lon2) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0.0;
+        }
+        else {
+            double theta = lon1 - lon2;
+            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+            dist = dist * 1.609344;
 
-        Double result = Math.acos(Math.sin(Math.toRadians(latA))*Math.sin(Math.toRadians(latB))+Math.cos(Math.toRadians(latA))*Math.cos(Math.toRadians(latB))*(Math.toRadians(lonB)-Math.toRadians(lonA)));
-        System.out.println(result);
-        return result;
+            return (dist);
+        }
+
     }
 
   /*

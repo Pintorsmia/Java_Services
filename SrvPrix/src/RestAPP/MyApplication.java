6f24c9237014656377809e0260b5210857f8e7b8
Janin Damien <damien.janin@soprasteria.com>
2020-02-27 15:51:40 +0100

    Ajout du prix.

--- SrvPrix/src/RestAPP/MyApplication.java
+++ SrvPrix/src/RestAPP/MyApplication.java
@@ -1,4 +1,17 @@
 package RestAPP;
 
-public class MyApplication {
+import prix.CalculPrix;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/API/")
+    public class MyApplication extends Application {
+        public Set<Class<?>> getClasses() {
+            HashSet h = new HashSet<Class<?>>();
+            h.add(CalculPrix.class);
+            return h;
+        }
 }
+
--- SrvPrix/src/prix/CalculPrix.java
+++ SrvPrix/src/prix/CalculPrix.java
@@ -1,4 +1,53 @@
 package prix;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("/calcul")
 public class CalculPrix {
+    private int prixkm = 5; //prix au km en €
+    private double dollar = 1.1;
+    private double francsuisse = 1.06;
+    private double bitcoin = 0.00012;
+
+        @GET
+        @Path("/test")
+        @Produces("text/plain")
+        public String test (){
+            return "test";
+        }
+
+
+        @GET
+        @Path("{distance},{devise}")
+        @Produces("text/plain")
+        public String getCalcul(@PathParam("distance") double distance, @PathParam("devise") String devise) {
+
+            double result = 0;
+            double prixtmp = distance * this.prixkm;
+
+
+            switch (devise.toLowerCase()){
+                case "euro":
+                    result = prixtmp;
+                    break;
+                case "dollar":
+                    result = this.dollar * prixtmp;
+                    break;
+                case "franc suisse":
+                    result = this.francsuisse * prixtmp;
+                    break;
+                case "bitcoin":
+                    result = this.bitcoin * prixtmp;
+                    break;
+                default:
+                    return "Erreur devise inconnue";
+            }
+            return Double.toString(result) + " " + devise ;
+
+
+// Exemple d'url --> http://item-s83074:8080/SrvPrix_war3315487828590068401/API/calcul/12,euro
+    }
 }
